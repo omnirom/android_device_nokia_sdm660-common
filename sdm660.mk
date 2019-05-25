86f31a39998f75dfe8a14c8169e6d396ea274456
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-24 21:04:19 -0700

    sdm660-common: Configure AVB
    * Build disabled VBMeta image and pack it into payload.bin.
    
    Change-Id: If3d53ad70c960fe1db85bbebb356062088ee8ce0

--- sdm660.mk
+++ sdm660.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2018 The Android Open-Source Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -35,7 +36,8 @@ AB_OTA_UPDATER := true
 
 AB_OTA_PARTITIONS += \
     boot \
-    system
+    system \
+    vbmeta
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -148,10 +150,6 @@ PRODUCT_PACKAGES += \
     rcs_service_aidl.xml \
     rcs_service_api \
     rcs_service_api.xml
-
-# Verity
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/system
-$(call inherit-product, build/target/product/verity.mk)
 
 # Vendor default.prop
 PRODUCT_COPY_FILES += \
